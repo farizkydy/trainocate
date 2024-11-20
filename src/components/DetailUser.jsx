2c8068d3746333b286f5ec56e6c3d0aec884f8bb
Fariz <fariz.rizaldy@salt.co.id>
2024-11-20 16:26:15 +0700

    add logicnumericId);

--- src/components/DetailUser.jsx
+++ src/components/DetailUser.jsx
@@ -4,17 +4,33 @@ function DetailUser() {
   const {id} = useParams()
 
 
-  // const data = [
-  //   { id: 1, name: "Artikel 1" },
-  //   { id: 2, name: "Artikel 2" }
-  // ];
+  const data = [
+    { id: 1, name: "Artikel 1" },
+    { id: 2, name: "Artikel 2" }
+  ];
 
+ // Konversi id dari useParams menjadi number
+ const numericId = parseInt(id, 10);
 
-  // todo cari apakah id dari useParams ada atau tidak di dalam state daata
-  return (
-   
-    <h1> User {id}</h1>
-  )
+ // Cari data berdasarkan id
+ const user = data.find((item) => item.id === numericId);
+ console.log(user);
+ console.log(numericId)
+
+ // Render
+ return (
+   <div>
+     {user ? (
+       <div>
+         <h1>Detail User</h1>
+         <p>ID: {user.id}</p>
+         <p>Name: {user.name}</p>
+       </div>
+     ) : (
+       <h1>Error: Data dengan ID {id} tidak ditemukan</h1>
+     )}
+   </div>
+ );
 }
 
 export default DetailUser;
