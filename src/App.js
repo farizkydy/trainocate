fac2f199c5c3f4105e56c3e50c6bfae4d0a17586
Fariz <fariz.rizaldy@salt.co.id>
2024-11-20 11:41:58 +0700

    day 2 sesi 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import Children from './children';
 
 function WelcomeMessage({isLoggedIn}) {
@@ -9,30 +9,81 @@ function WelcomeMessage({isLoggedIn}) {
 
 
 function App() {
-  const [name, setName] = useState(" Mr.")
+  const [name, setName] = useState("")
+  const [gender, setGender] = useState("male")
+  const inputRef = useRef();
+  const [error, setError] = useState("")
+  const [dataUser, setDataUser] = useState([])
+  const [checkBox, setCheckBox] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    alert(`Nama anda terdaftar sebagai ${name}`);
+    if (name.length < 3) {
+      setError('Please input minimal 3 characters')
+    } else if (!gender) {
+      setError('Please choose a gender')
+    } else {
+      setError('')
+      setName('')
+      setGender('male')
+      const newUser = {
+        name,
+        gender
+      }
+      setCheckBox(!checkBox)
+      setDataUser([...dataUser, newUser])
+    }
   };
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
         <label>
           Gender:
-          <input type="radio" name="gender" value="male" />
+          <input type="radio" name="gender" value="male" 
+          onChange={(e) => setGender(e.target.value)}/>
           Male  
-          <input type="radio" name="gender" value="female" />
+          <input type="radio" name="gender" value="female" 
+          onChange={(e) => setGender(e.target.value)}/>
           Female
         </label>
         <label>
           Name:
-          <input type="text" name="name" value={name} 
+          <input type="text" name="name" value={name} placeholder='Input Your Full name'
           onChange={(e) => setName(e.target.value)}
           />
+          {error && <p style={{color: 'red'}}>{error}</p>}
+        </label>
+        <label>
+          Terms of Service
+          <input type="checkbox" name="terms" value={checkBox} />
+          {checkBox ? "Agree" : "Not Agree"}
         </label>
         <button type="submit">Submit</button>
       </form>
+
+      <h2>List Users</h2>
+      {dataUser.length > 0 ? (
+        <table className="table">
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Name</th>
+              <th>Gender</th>
+            </tr>
+          </thead>
+          <tbody>
+            {dataUser.map((item, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{item.name}</td>
+                <td>{item.gender}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <h3>There is no data</h3>
+      )} 
     </div>
   );
 }
